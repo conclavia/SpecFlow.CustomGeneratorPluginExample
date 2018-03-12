35b5e5bc793d370698972522b25d763b01d796f4
Wayne X Turner <wayne_x_turner@amp.com.au>
2018-03-13 09:37:36 +1100

    Add example plugin

---
+++ MyGenerator.SpecFlowPlugin/MyGeneratorPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoDi;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+
+namespace MyGenerator.SpecFlowPlugin
+{
+    public class MyGeneratorPlugin : IGeneratorPlugin
+    {
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration) { }
+
+        public void RegisterCustomizations(ObjectContainer container,
+            SpecFlowProjectConfiguration generatorConfiguration)
+        { }
+
+        public void RegisterDependencies(ObjectContainer container) { }
+    }
+}
---
+++ TestProject/MySteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace TestProject
+{
+    [Binding]
+    public class MySteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreens(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
